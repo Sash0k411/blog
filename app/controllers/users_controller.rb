c575c0789af5fe71a3779382a6f175e052cf7a42
Alexander <alexx.blokhin@gmail.com>
2023-06-09 22:52:08 +0300

    init

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: 'User was successfully destroyed.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :password)
+  end
+end
