03dd717382e67abf898b609e574d2fc8fe412d8a
Alexander <alexx.blokhin@gmail.com>
2023-06-13 21:03:06 +0300

    Main functional

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
   root "posts#index"
   resources :posts
   resources :users, only: [:index, :edit, :update, :destroy]
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create!(username: 'defaultuser', email: 'support@test.com', password: 'support', password_confirmation: 'support')
+User.create!(username: 'standartuser', email: 'standart@test.com', password: 'testtest', password_confirmation: 'testtest')
